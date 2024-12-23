0f2652f1de6ccc1b506dd961bd197da1d304475d
NguyenBat-Phuong <nguyenbatphuong@gmail.com>
2024-12-23 20:26:50 +0700

    not thing

--- db.js
+++ db.js
@@ -2,13 +2,18 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 // Tạo kết nối MySQL
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   // password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+// });
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: "localhost",
+  user: "root",
+  password: "1",
+  database: "library",
 });
-
 // Kết nối cơ sở dữ liệu
 connection.connect((err) => {
   if (err) {
